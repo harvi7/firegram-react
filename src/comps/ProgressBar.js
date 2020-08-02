cab3d7b9e339e2bad66a7dc96d402579d0fb2f99
Harsh Virani <iamharshvirani7@gmail.com>
2020-08-01 23:43:47 -0700

    Creating modal and Added Animations

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,18 +1,22 @@
 import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
+import { motion } from 'framer-motion';
 
 const ProgressBar = ({ file, setFile }) => {
-    const { url, progress } = useStorage(file);
-    
-    useEffect(() => {
-        if (url) {
-            setFile(null);
-        }
-    }, [url, setFile])
+  const { progress, url } = useStorage(file);
 
-    return (
-        <div className="progress-bar" style={{ width: progress + '%' }}></div>
-    )
-}
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+
+  return (
+    <motion.div className="progress-bar"
+      initial={{ width: 0 }}
+      animate={{ width: progress + '%' }}
+    ></motion.div>
+  );
+} 
 
 export default ProgressBar;
